e9d80f7d945c65b29e222241d32e105155d743cf
puttehi <peteeez95@gmail.com>
2024-04-11 19:14:14 +0300

    Pass in cached palette to highlighter

--- lua/nvim-puttehi-dark/init.lua
+++ lua/nvim-puttehi-dark/init.lua
@@ -4,8 +4,9 @@ M = {}
 ---@class ColorPluginOpts Overrides for default options
 ---@field colors? HighlightConfig Any highlight configuration overrides (untested)
 
+---@param palette Palette Palette to use for lookups if color was not already valid
 ---@param color ColorCode | string #RRGGBB, "none", "NONE" or vim color code like "Pink"
-local function parse_color(color)
+local function parse_color(palette, color)
     local color_type = type(color)
     if color_type ~= "string" then
         print(vim.inspect({ "invalid color", color_type, color }))
@@ -21,10 +22,10 @@ local function parse_color(color)
         return "none"
     end
 
-    local palette_color = require("puttehi_dark.palette")[color]
+    local palette_color = palette[color]
     if palette_color then
         -- Was a palette group name, return underlying #RRGGBB
-        return parse_color(palette_color)
+        return parse_color(palette, palette_color)
     end
 
     -- Was not #RRGGBB and not found in palette -> Try to find named color from nvim
@@ -33,11 +34,11 @@ end
 
 ---@param hl HighlightKey VIM highlight to set
 ---@param value HighlightValue Highlight configuration to use
-local function apply_highlight(hl, value)
+function M.apply_highlight(hl, value)
     local style = value.style and "gui=" .. value.style or "gui=NONE"
-    local fg = value.fg and "guifg=" .. parse_color(value.fg) or "guifg=NONE"
-    local bg = value.bg and "guibg=" .. parse_color(value.bg) or "guibg=NONE"
-    local sp = value.sp and "guisp=" .. parse_color(value.sp) or ""
+    local fg = value.fg and "guifg=" .. parse_color(M.palette, value.fg) or "guifg=NONE"
+    local bg = value.bg and "guibg=" .. parse_color(M.palette, value.bg) or "guibg=NONE"
+    local sp = value.sp and "guisp=" .. parse_color(M.palette, value.sp) or ""
 
     vim.cmd(string.format("highlight %s %s %s %s %s", hl, style, fg, bg, sp))
 
@@ -69,7 +70,7 @@ function M.apply()
 
     -- Set new highlights
     for group_key, palette_key in pairs(M.colors) do
-        apply_highlight(group_key, palette_key)
+        M.apply_highlight(group_key, palette_key)
     end
 end
 
